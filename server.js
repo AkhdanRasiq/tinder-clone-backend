d7009b26c86a914828f2f80f9db38ead3a17a5f0
GenZay <indan.gumelar20@gmail.com>
2022-02-03 12:17:52 +0800

    update connection url

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import Cards from './dbCards.js'
 // APP CONFIG
 const app               = express()
 const port              = process.env.PORT || 8001
-const connection_url    = '{PASTE_YOUR_CONNECTION_URL_HERE}'
+const connection_url    = '{PASTE_YOUR_MONGODB_CONNECTION_URL_HERE}'
 
 // MIDDLEWARE
 app.use(express.json())
